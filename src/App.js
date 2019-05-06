5440b811df95f59b9240c46a200cd0954a155ede
Tanner Scadden <tanner@scaddenfamily.com>
2019-05-06 15:20:24 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import CurrencyConverter from './Components/CurrencyConverter';
+
 class App extends Component {
   render() {
     return (
       <div>
-
+        <CurrencyConverter render={(currencyData, amount) => (
+          <p>
+            US Dollar ${amount.toFixed(2)} - {currencyData.name}{' '}
+            {currencyData.symbol}
+            {(amount * currencyData.rate).toFixed(2)}
+          </p>
+        )}/>
       </div>
     );
   }
